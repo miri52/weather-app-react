30e6968a1b718b8763108a99ebe14f8397dcfbbc
miri52 <miri52@users.noreply.github.com>
2020-04-27 17:43:25 +0100

    Update the proverb using mapping istead of switch
    - add proverbs for all main descriptions from API
    - use mapping for better readability

--- src/Proverb.js
+++ src/Proverb.js
@@ -28,84 +28,47 @@ export default function Proverb(props) {
       saying: "Words are the fog one has to see through.",
       author: "-Zen proverb",
     },
-    default: {
-      saying: "After bad weather comes good weather.",
-      author: "-Maltese Proverb",
+    dust: {
+      saying: "Gather dust to build a mountain.",
+      author: "-Korean proverb",
+    },
+    smoke: {
+      saying: "Smoke never rises without fire",
+      author: "-Haitian proverb",
+    },
+    sand: {
+      saying: "Knowledge without wisdom is like water in the sand",
+      author: "-Guinean proverb ",
     },
   };
 
-  switch (props.weatherDescription.toLowerCase()) {
-    case "clouds":
-      return (
-        <p className="proverb-section">
-          <span className="proverb">
-            {proverbs.clouds.saying}
-            <br />
-          </span>
-          <span className="proverb-author">{proverbs.clouds.author}</span>
-        </p>
-      );
-    case "snow":
-      return (
-        <p className="proverb-section">
-          <span className="proverb">
-            {proverbs.snow.saying}
-            <br />
-          </span>
-          <span className="proverb-author">{proverbs.snow.author}</span>
-        </p>
-      );
-    case "rain":
-    case "drizzle":
-      return (
-        <p className="proverb-section">
-          <span className="proverb">
-            {proverbs.rain.saying}
-            <br />
-          </span>
-          <span className="proverb-author">{proverbs.rain.author}</span>
-        </p>
-      );
-    case "clear":
-      return (
-        <p className="proverb-section">
-          <span className="proverb">
-            {proverbs.sun.saying}
-            <br />
-          </span>
-          <span className="proverb-author">{proverbs.sun.author}</span>
-        </p>
-      );
-    case "thunderstorm":
-      return (
-        <p className="proverb-section">
-          <span className="proverb">
-            {proverbs.storm.saying}
-            <br />
-          </span>
-          <span className="proverb-author">{proverbs.storm.author}</span>
-        </p>
-      );
-    case "mist":
-    case "fog":
-      return (
-        <p className="proverb-section">
-          <span className="proverb">
-            {proverbs.mist.saying}
-            <br />
-          </span>
-          <span className="proverb-author">{proverbs.mist.author}</span>
-        </p>
-      );
-    default:
-      return (
-        <p className="proverb-section">
-          <span className="proverb">
-            {proverbs.default.saying}
-            <br />
-          </span>
-          <span className="proverb-author">{proverbs.default.author}</span>
-        </p>
-      );
-  }
+  const proverbMapping = {
+    snow: proverbs.snow,
+    clouds: proverbs.clouds,
+    clear: proverbs.sun,
+    rain: proverbs.rain,
+    drizzle: proverbs.rain,
+    thunderstorn: proverbs.storm,
+    squall: proverbs.storm,
+    tornado: proverbs.storm,
+    mist: proverbs.mist,
+    fog: proverbs.mist,
+    haze: proverbs.mist,
+    dust: proverbs.dust,
+    smoke: proverbs.smoke,
+    ash: proverbs.smoke,
+    sand: proverbs.sand,
+  };
+
+  return (
+    <p className="proverb-section">
+      <span className="proverb">
+        {proverbMapping[props.weatherDescription.toLowerCase()].saying}
+        <br />
+      </span>
+      <span className="proverb-author">
+        {proverbMapping[props.weatherDescription.toLowerCase()].author}
+      </span>
+    </p>
+  );
 }
